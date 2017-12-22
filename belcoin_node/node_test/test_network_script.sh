b16f8b22fe88e0cab7417f063e6540c98ab7d8e3
pifragile <pigu69@gmail.com>
2017-12-22 14:21:20 +0100

    change path in test network script

--- belcoin_node/node_test/test_network_script.sh
+++ belcoin_node/node_test/test_network_script.sh
@@ -7,9 +7,9 @@ mkdir output
 
 pkill -f -9 node
 
-python belcoin_node/node.py 0 7080 http://127.0.0.1:7081,http://127.0.0.1:7082 localhost:27870 localhost:27871,localhost:27872 50050 &>> output/0.txt
-python belcoin_node/node.py 1 7081 http://127.0.0.1:7080,http://127.0.0.1:7082 localhost:27871 localhost:27870,localhost:27872 50051 &>> output/0.txt
-python belcoin_node/node.py 2 7082 http://127.0.0.1:7080,http://127.0.0.1:7081 localhost:27872 localhost:27870,localhost:27871 50052 &>> output/0.txt
+python ../node.py 0 7080 http://127.0.0.1:7081,http://127.0.0.1:7082 localhost:27870 localhost:27871,localhost:27872 50050 &>> output/0.txt
+python ../node.py 1 7081 http://127.0.0.1:7080,http://127.0.0.1:7082 localhost:27871 localhost:27870,localhost:27872 50051 &>> output/0.txt
+python ../node.py 2 7082 http://127.0.0.1:7080,http://127.0.0.1:7081 localhost:27872 localhost:27870,localhost:27871 50052 &>> output/0.txt
 
 sleep 5
 
